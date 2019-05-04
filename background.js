972ab8eca91e3ee0b45e24deab73bc0e69de856b
suchwalko <541795+suchwalko@users.noreply.github.com>
2019-05-04 20:03:40 +0200

    refactor, add links rewriting and undo

--- background.js
+++ background.js
@@ -1,54 +1,102 @@
 const icons = {
     on: 'icons/browser-orange.png',
     off: 'icons/browser-grayscale.png'
-}
+};
 
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.storage.local.get('enabled', (keys) => {
-        if (keys.enabled) {
-            chrome.storage.local.set({ enabled: false });
-            chrome.browserAction.setIcon({ path: icons.off });
-            return;
-        }
-        chrome.storage.local.set({ enabled: true });
+const navigationSources = new Map();
+const myTabs = new Set();
+
+const sendMessage = (message) => {
+    chrome.tabs.query({}, function (tabs) {
+        tabs.forEach(tab => {
+            chrome.tabs.sendMessage(tab.id, message);
+        });
+    });
+};
+
+const enable = () => {
+    chrome.storage.local.set({ enabled: true });
+    chrome.browserAction.setIcon({ path: icons.on });
+    sendMessage('enabled');
+};
+
+const disable = () => {
+    chrome.storage.local.set({ enabled: false });
+    chrome.browserAction.setIcon({ path: icons.off });
+    sendMessage('disabled');
+};
+
+const isEnabled = () => {
+    return new Promise(resolve => {
+        chrome.storage.local.get('enabled', (keys) => {
+            if (keys.enabled) {
+                resolve(true);
+                return;
+            }
+            resolve(false)
+        })
+    });
+};
+
+const initializeIcon = async () => {
+    if (await isEnabled()) {
         chrome.browserAction.setIcon({ path: icons.on });
-    })
-});
+        return;
+    }
+    chrome.browserAction.setIcon({ path: icons.off });
+};
 
-var navigationSources = new Map();
-var myTabs = new Set();
+const getActiveTabId = () => {
+    return new Promise(resolve => {
+        chrome.tabs.query({ currentWindow: true, active: true },
+            function (activeTabs) {
+                const activeTab = activeTabs[0];
+                if (!activeTab || !activeTab.id) {
+                    resolve(null);
+                }
+                resolve(activeTab.id);
+            }
+        );
+    });
+};
+
+chrome.browserAction.onClicked.addListener(async () => {
+    const enabled = await isEnabled();
+    if (enabled) {
+        disable();
+        return;
+    }
+    enable();
+});
 
 chrome.webNavigation.onCreatedNavigationTarget.addListener((details) => {
     navigationSources.set(details.tabId, details.sourceTabId);
 });
 
-chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
     const targetTabId = details.tabId;
     if (navigationSources.has(targetTabId)) {
         return;
     }
-    chrome.tabs.query({ currentWindow: true, active: true },
-        function (activeTabs) {
-            const activeTab = activeTabs[0];
-            if(!activeTab) {
-                return;
-            }
-            navigationSources.set(targetTabId, activeTab && activeTab.id);
-        }
-    );
+
+    const at = await getActiveTabId();
+    if (getActiveTabId === null) {
+        return;
+    }
+    navigationSources.set(targetTabId, getActiveTabId);
 });
 
 chrome.webNavigation.onCommitted.addListener((details) => {
     const tabId = details.tabId;
     const sourceTabId = navigationSources.get(tabId);
-    const newTab = tabId !== sourceTabId;
-    const myTab = myTabs.has(tabId);
+    const isNewTab = tabId !== sourceTabId;
+    const isMyTab = myTabs.has(tabId);
     const url = details.url;
     const transitionType = details.transitionType;
     const transitionQualifiers = details.transitionQualifiers;
     const hasTransitionQualifiers = !!transitionQualifiers.length;
 
-    if (newTab || myTab || !['link', 'auto_bookmark'].includes(transitionType) || hasTransitionQualifiers) {
+    if (isNewTab || isMyTab || !['link', 'auto_bookmark'].includes(transitionType) || hasTransitionQualifiers) {
         return;
     }
 
@@ -68,36 +116,6 @@ chrome.webNavigation.onCompleted.addListener((details) => {
     navigationSources.delete(details.tabId);
 });
 
-// chrome.history.onVisited.addListener((historyItem) => {
-//     if (!historyItem.url) return;
-//     var lastVisitTime = historyItem.lastVisitTime;
-//     chrome.history.getVisits({ url: historyItem.url }, (visitItems) => {
-//         var currentVisit = visitItems.find(v => v.visitTime === lastVisitTime);
-//         /* https://developers.chrome.com/extensions/history#transition_types
-//         "link"	The user got to this page by clicking a link on another page.
-//         "auto_bookmark"	The user got to this page through a suggestion in the UI — for example, through a menu item.
-//         */
-//         if (['link', 'auto_bookmark'].includes(currentVisit.transition)) {
-//             chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-//                 var activeTab = tabs[0];
-//                 var activeTabId = activeTab.id;           
-//              });
-//         }
-//     });
-// });
-
-
-// https://developers.chrome.com/extensions/webNavigation#event-onCommitted
-// openerTabId: 2201
-// visitTime: 1556831792055.313
-// lastVisitTime: 1556831963651.305
-//chrome.tabs.create({ url: newURL });
-
-
-
-
-
-/* check this:
-https://developers.chrome.com/extensions/history
-https://developers.chrome.com/extensions/history#type-HistoryItem
-*/
+(async () => {
+    await initializeIcon();
+})();
--- script.js
+++ script.js
@@ -1,53 +1,30 @@
-// (function () {
-//     /**
-//      * @param {string} url 
-//      */
-//     const isLocalUrl = (url) => {
-//         const currentPage = window.location.protocol + '//' + window.location.hostname;
+(function () {
+    var links = document.querySelectorAll('a[href]:not([target]), a[href]:not([download])');
 
-//         var relative = url.indexOf('http') !== 0;
-//         var absolute = url.indexOf(currentPage) === 0;
+    const run = () => {        
+        for (const link of links) {
+            link.setAttribute('target', '_blank');
+        }
+    }
 
-//         return relative || absolute;
-//     }
+    const undo = () => {
+        for (const link of links) {
+            link.removeAttribute('target');
+        }
+    }
 
-//     const links = document.querySelectorAll('a[href]');
+    chrome.storage.local.get('enabled', (keys) => {
+        if(keys.enabled) {
+            run();
+        }
+    });
 
-//     const run = () => {
-//         for (const link of links) {
-//             //if(isLocalUrl(link.href)) {
-//             //    continue;
-//             //}
-//             link.setAttribute('target', '_blank');
-//         }
-    
-//         var navigationType = performance.getEntriesByType("navigation")[0].type;
-    
-//         if (navigationType == 'navigate' && history.length > 1) {
-//             setTimeout(() => {
-//                 console.log(navigationType);
-//                 console.log(history)
-//                 window.stop();
-//                 const currentUrl = window.location.href;
-//                 window.onbeforeunload = (e) => { window.open(currentUrl, '_blank'); return undefined; };
-//                 history.back();
-//             }, 5000);            
-//         }
-//     }
-
-//     chrome.storage.local.get('enabled', (keys) => {
-//         return;
-//         if(keys.enabled) {
-//             run();
-//         }
-//     });    
-// })();
-
-/*
-performance.getEntriesByType("navigation")[0].type
-"back_forward" "reload" "navigate"
-
-history.length > 1
-window.blur += () => history.back()
-window.onbeforeunload = (e) => { window.open('http://abc.com', '_blank'); return undefined; }
-*/
+    chrome.runtime.onMessage.addListener((request, sender) => {
+        if(request === 'enabled') {
+            run();
+        }
+        if(request === 'disabled') {
+            undo();
+        }
+    })
+})();
